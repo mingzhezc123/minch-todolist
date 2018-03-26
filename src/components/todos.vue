dffc4aa3647510ac8c86ea4d3ad1a846bfea5add
mingzhezc123 <1095889023@qq.com>
2018-03-27 00:55:28 +0800

    initial second
    修改添加依赖和项目架构

---
+++ src/components/todos.vue
@@ -0,0 +1,42 @@
+<template>
+  <section id="todo-app">
+    <header class="todo-header">
+      <h2>Minch备忘录</h2>
+      <input class="new-toSdo" type="text" placeholder="记录你要做的事^_^" >
+    </header>
+    <section class="todo-content">
+
+    </section>
+    <footer class="todo-footer">
+
+    </footer>
+  </section>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+    watch: {
+
+    },
+    directives: {
+
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~common/styles/reset'
+  #todo-app
+    text-align center
+</style>
